5fcaca1019c6412e63dbab21bda07990cf2bc01e
Kris Tantinirundr <kris.percussion@gmail.com>
2024-06-14 13:22:58 +0700

    Update page.tsx

--- app/(dashboard)/game/page.tsx
+++ app/(dashboard)/game/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Unity unityProvider={unityProvider} style={{ width: 440 , height: 600}}/>
+        <Unity unityProvider={unityProvider} style={{ width: 340 , height: 600}}/>
     </main>
 
   )
